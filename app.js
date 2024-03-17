e937798bf98e52ef9ab81c30dbc453eca22aeffa
Shira-Abrams <sh3000444@gmail.com>
2024-03-17 21:41:13 +0200

    book server

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express=require('express')
-
+const morgan=require('morgan')
+const cors=require('cors');
 const userRoute=require('./router/user.route')
 const bookROute=require('./router/book.route')
+const {default:mongoose}=require('mongoose');
+const config=require('./config')
 const app=express();
-
+mongoose.connect(config.DB_URL)
+.then(()=>console.log('conected to mong db '))
+.catch(error=>console.log(error))
 app.use(express.json()) // אם שולחים אוביקט מהקליינט
 app.use(express.urlencoded({ extended: false })) // אם שולחים קבצים מהקליינט
-
+app.use(morgan('dev'));//printing the information of the request 
+app.use(cors())//enable access to all api's addresses
 app.use('/user',userRoute)
 app.use('/book',bookROute)
+app.get("/", (req, res) => {
+    res.send("wellcome");
+  });
 const port=5000;
 app.listen(port,()=>{
     console.log(`listening on http://localhost:${port}`);
--- router/book.route.js
+++ router/book.route.js
@@ -1,62 +1,16 @@
-const express = require('express');
 
-const book=express.Router()
-const {Books}=require('../book')
-
-
-book.post( "/", (req,res,next)=>{
-   const newBook=req.body;
-   console.log(newBook);
-   let lastId=Books[Books.length-1].id+1
-
-   if(isNaN(lastId))
-   lastId=100;
-   newBook.id=lastId 
-   Books.push(newBook)
-   res.status(200).send(newBook)
-
-})
-
-
-book.put("/:id",(req,res)=>{
-    const id=+req.params.id;
-    if(id!=req.body.id)
-    res.status(409).json({error:'make sure the id book are similar'})
-    const updatBook=Books.find(x=>x.id==id);
-
-    if(updatBook)
-    {
-        updatBook.auther=req.body.auther||updatBook.auther;
-        updatBook.name=req.body.name||updatBook.name
-        res.send(updatBook)
-
-    }
-    res.status(404).json({error:'book bo fuond'})
-})
-
-book.get( "/" ,(req,res)=>{
-
-    res.send(Books)
-})
-
-
-book.get( "/:id" ,(req,res)=>{
-
-
-    const {id}=req.body;//another way to extract  the id form the body request  by curly braket 
-    const getBook=Books.find(x=>x.id==id)
-    if(getBook)
-    res.send(getBook)
-   res.status(404).json({error:'non found user !!'})
-})
-
-book.delete("/:id",(req,res)=>{
-
-    const id=+req.body.id;
-    const deletedBook=Books.findIndex(x=>x.id==id);
-    if(deletedBook)
-    res.send('ok 200 was deleted succesfully')
-   else
-   res.status(404).json({error:'book not found '})
-})
- module.exports=book
+const express=require('express');
+const{
+    GetAllBook,
+    DeleteBook,
+    GetBookById,
+    AddBook,
+    UpdateBook
+}=require('../controllers/book.controller')
+const router=express.Router();
+router.get('/',GetAllBook);
+router.get('/:id',GetBookById);
+router.post('/',AddBook);
+router.put('/:id',UpdateBook);
+router.delete('/:id',DeleteBook);
+module.exports=router;
